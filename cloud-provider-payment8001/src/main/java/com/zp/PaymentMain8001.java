87843ba1e1051c9661258d640b5dbf7e42206e93
zhengpanone <zhengpanone@hotmail.com>
2021-10-30 00:21:11 +0800

    eureka client 配置

--- cloud-provider-payment8001/src/main/java/com/zp/PaymentMain8001.java
+++ cloud-provider-payment8001/src/main/java/com/zp/PaymentMain8001.java
@@ -2,6 +2,7 @@ package com.zp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
  * Main
@@ -10,6 +11,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * @since 2021-10-21
  */
 @SpringBootApplication
+@EnableEurekaClient
 public class PaymentMain8001 {
     public static void main(String[] args) {
         SpringApplication.run(PaymentMain8001.class,args);
