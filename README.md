63da0e911cbb17bd2cd33e26f9ab8031ac46b183
zhengpanone <zhengpanone@hotmail.com>
2021-11-02 18:12:34 +0800

    服务提供8001，服务消费80注册到eureka集群

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # spring-cloud-learn
 spring cloud learn
+
+修改映射配置添加到hosts文件
+
+C:\Windows\System32\drivers\etc\hosts
+
+127.0.0.1 eureka7001.com 
+
+127.0.0.1 eureka7002.com
---
+++ cloud-eureka-server7002/src/main/java/com/zp/EurekaMain7002.java
@@ -0,0 +1,19 @@
+package com.zp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * Main7002
+ *
+ * @author zhengpanone
+ * @since 2021-11-01
+ */
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaMain7002 {
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaMain7002.class,args);
+    }
+}
