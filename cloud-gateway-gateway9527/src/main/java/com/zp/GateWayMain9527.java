677f4dfdf835dcfb33ed77390c2c4d651a1c2a11
zhengpanone <zhengpanone@htomail.com>
2024-01-13 10:17:36 +0800

    网关

---
+++ cloud-gateway-gateway9527/src/main/java/com/zp/GateWayMain9527.java
@@ -0,0 +1,19 @@
+package com.zp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * Author : zhengpanone
+ * Date : 2024/1/13 09:48
+ * Version : v1.0.0
+ * Description: TODO
+ */
+@SpringBootApplication
+@EnableDiscoveryClient
+public class GateWayMain9527 {
+    public static void main(String[] args) {
+        SpringApplication.run(GateWayMain9527.class, args);
+    }
+}
